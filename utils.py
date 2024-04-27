c2960ec31b05c8d95b2faf6899585e3cc9849eea
adrianferu1994 <adrianfelipe010494@gmail.com>
2024-04-27 09:28:55 -0500

    Modificamos el script utils.py y creamos el archivo de 2_dataAnalysis.ipynb

--- utils.py
+++ utils.py
@@ -17,3 +17,17 @@ def get_coord(Address,YOUR_API_KEY):
     except:
         results = ['NotFound','NA','NA']
     return results 
+
+
+#We will create a function based on the score column. The syntax will be the following:
+#if score >= 4.5, then result will be '45.-5.0'
+#if score >=4.0 and <4.5, then result will be '4.0-4.5'
+#if score <4.0. then result will be '< 4.0'
+def get_score_range(score):
+    if score >= 4.5:
+        result = '4.5-5.0'
+    elif score >= 4.0 and score < 4.5:
+        result = '4.0-4.7'
+    else:
+        result = '< 4.0'
+    return result
