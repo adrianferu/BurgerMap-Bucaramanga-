ce5ac76b176009db4e8ac0174bb4cf16d275982e
adrianferu1994 <adrianfelipe010494@gmail.com>
2024-04-27 14:55:42 -0500

    cambios leves en la visualización

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def get_score_range(score):
     if score >= 4.5:
         result = '4.5-5.0'
     elif score >= 4.0 and score < 4.5:
-        result = '4.0-4.7'
+        result = '4.0-4.5'
     else:
         result = '< 4.0'
     return result
